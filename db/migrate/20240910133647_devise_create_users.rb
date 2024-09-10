ea975d20b302e16a4c82607bfea9223d75b631cf
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-10 11:03:16 -0300

    Adicionado as validações de tamanho de nome no model User e adicionado a validação no migration que não permite nomes nulos.

--- db/migrate/20240910133647_devise_create_users.rb
+++ db/migrate/20240910133647_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name
+      t.string :name null: false, default: ""
 
       t.timestamps null: false
     end
