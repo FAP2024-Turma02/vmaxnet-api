c44962970348cce1a34be1378fa1a317fca07505
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-12 09:49:37 -0300

    Adicionada a validação de presence no lado do model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, length: { minimum: 3 }
+         validates :name, presence: true, length: { minimum: 3 }
 end
