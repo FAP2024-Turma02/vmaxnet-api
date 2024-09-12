ff4bf751892b52c9da6403815caf874ee2023525
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-12 09:43:15 -0300

    Corrigido um erro de sintaxe da migrate e removido o valor defualt do campo name.

--- db/migrate/20240910133647_devise_create_users.rb
+++ db/migrate/20240910133647_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name null: false, default: ""
+      t.string :name, null: false
 
       t.timestamps null: false
     end
