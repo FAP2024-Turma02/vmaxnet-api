392cfcbe346a5c4dfe17fb4c47c12c46a09b12ab
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-19 11:11:32 -0300

    Corrigido a estrutura do model contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,17 +1,15 @@
 class Contact < ApplicationRecord
-  enum contact_type: { email: 0, phone: 1 }
 
-  validates :contact_type, :contact, presence: true
-  validates :contact, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email' }, if: :email?
-  validates :contact, format: { with: /\A\d{8,9}\z/, message: 'must be a valid phone number' }, if: :phone?
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
+  validates :phone, format: { with: /\A\d{8,9}\z/ }, allow_blank: true
+
+  validate :email_or_phone_present
 
   private
 
-  def email?
-    contact_type == 'email'
-  end
-
-  def phone?
-    contact_type == 'phone'
+  def email_or_phone_present
+    if email.blank? && phone.blank?
+      errors.add(:base, "Either email or phone must be present")
+    end
   end
 end
--- db/migrate/20240918204655_create_contacts.rb
+++ db/migrate/20240918204655_create_contacts.rb
@@ -1,8 +1,8 @@
 class CreateContacts < ActiveRecord::Migration[7.0]
   def change
     create_table :contacts do |t|
-      t.integer :contact_type, null: false
-      t.string :contact, null: false
+      t.string :email, null: true
+      t.string :phone, null: true
 
       t.timestamps
     end
