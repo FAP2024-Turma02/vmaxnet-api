eb07a1a30a068a950a725b08f7bc59d6312086fb
atanael lima <xplayerdownloadx@gmail.com>
2024-09-19 10:29:44 -0300

    correção dos conflitos

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.1"
+gem 'base64'
+gem 'bigdecimal'
+gem 'mutex_m'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.8", ">= 7.0.8.1"
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,30 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_16_184248) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "produtos", force: :cascade do |t|
+    t.integer "contract_number", null: false
+    t.string "store_correspondent", null: false
+    t.string "cardholder", null: false
+    t.boolean "loyalty", default: false
+    t.string "equipment", null: false
+    t.string "installation_type", null: false
+    t.integer "installation_installments", null: false
+    t.string "installation_payment_method", null: false
+    t.boolean "exempt", default: false
+    t.string "billing_type", null: false
+    t.date "due_date", null: false
+    t.string "who_referred"
+    t.text "financial_comments"
+    t.text "technical_comments"
+    t.decimal "adhesion_total", precision: 10, scale: 2, null: false
+    t.decimal "rescission_total", precision: 10, scale: 2, null: false
+    t.decimal "monthly_total", precision: 10, scale: 2, null: false
+    t.date "start_date", null: false
+    t.date "end_date"
+    t.string "billing_address", null: false
+    t.string "installation_address", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
