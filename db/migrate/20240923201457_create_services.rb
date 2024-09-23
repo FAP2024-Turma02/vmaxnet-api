15fbe0b7a89984f94e0c3c8bdfae7ed438670857
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-09-23 17:16:24 -0300

    Adicionado validacoes na migrate

--- db/migrate/20240923201457_create_services.rb
+++ db/migrate/20240923201457_create_services.rb
@@ -1,13 +1,13 @@
 class CreateServices < ActiveRecord::Migration[7.0]
   def change
     create_table :services do |t|
-      t.string :name
-      t.string :group
-      t.decimal :monthly_fee
-      t.decimal :adhesion_fee
-      t.decimal :termination_fee
-      t.text :description
-      t.datetime :available_at
+      t.string :name, null: false
+      t.integer :group, null: false
+      t.decimal :monthly_fee, null: false
+      t.decimal :adhesion_fee, null: false
+      t.decimal :termination_fee, null: false
+      t.text :description, null: false
+      t.datetime :available_at, null: false
 
       t.timestamps
     end
