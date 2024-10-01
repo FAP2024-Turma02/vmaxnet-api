8575660efa7dc7e95b9e2dd8e68781e6caffed1a
AlansidneyJunior <alansidney.silva21@gmail.com>
2024-10-01 01:10:44 -0300

    Corrigindo erros, falta testar index de Endereco

--- app/models/corporate_client.rb
+++ app/models/corporate_client.rb
@@ -1,6 +1,6 @@
 class CorporateClient < ApplicationRecord
   
-  has_many enderecos, dependent :destroy
+  has_many enderecos, dependent: :destroy
   
   validates :company_name, :trade_name, :tax_id, :foundation_date, presence: true
   validates_cnpj :document
--- app/models/physical_client.rb
+++ app/models/physical_client.rb
@@ -1,10 +1,10 @@
 class PhysicalClient < ApplicationRecord
     
-    has_many :enderecos, dependent :destroy
+    has_many :enderecos, dependent: :destroy
     
     validates :name, length: { minimum: 8 }, presence: true
     validates :nickname, presence: false
-    validates :cpf, lentgh { is: 11 }, uniqueness: true, presence: true
+    validates :cpf, length: { is: 11 }, uniqueness: true, presence: true
     validates :rg, uniqueness: true, presence: true
     validates :date_of_birth, presence: true
 end
