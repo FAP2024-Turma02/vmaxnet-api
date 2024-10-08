3b81c82ea2d7bf4a8d05bb74a2b3fc1767708834
AlansidneyJunior <alansidney.silva21@gmail.com>
2024-10-08 01:54:43 -0300

    Criando Entidade Address com validações

---
+++ db/migrate/20241008044353_create_addresses.rb
@@ -0,0 +1,19 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.integer :address_type
+      t.string :street_name
+      t.string :reference
+      t.string :zip_code
+      t.string :city
+      t.string :neighborhood
+      t.string :address
+      t.string :number
+      t.string :complement
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_08_044353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "address_type"
+    t.string "street_name"
+    t.string "reference"
+    t.string "zip_code"
+    t.string "city"
+    t.string "neighborhood"
+    t.string "address"
+    t.string "number"
+    t.string "complement"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "referral_source"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
