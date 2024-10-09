f1c14367b9856b3aa70623b43e44151e997ee857
AlansidneyJunior <alansidney.silva21@gmail.com>
2024-10-09 00:54:34 -0300

    Adicionando e testando validações IndividualClient e CorporateClient.

--- app/models/individual_client.rb
+++ app/models/individual_client.rb
@@ -1,2 +1,8 @@
 class IndividualClient < Client
+    validates :name, presence: true
+    validates :nickname, presence: false
+    validates :cpf, presence: true, uniqueness: true
+    validates :rg, presence: true
+    validates :date_of_birth, presence: true
+    validates :how_find_us, presence: false
 end
