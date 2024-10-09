b9b5737145a8869c6fdacce095eb437fa5d96e70
AlansidneyJunior <alansidney.silva21@gmail.com>
2024-10-09 01:09:27 -0300

    Adicionando e testando rotas index e show das tabelas IndividualClient e CorporateClient e corrigindo rotas de Address

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :individual_clients, only: [:index, :show]
+  resources :corporate_clients, only: [:index, :show]
   resources :addresses, only: [:index, :show]
 end
