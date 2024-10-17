680445a4a49e18fa11d1363e98cc1fa1d012e2bd
AlansidneyJunior <alansidney.silva21@gmail.com>
2024-10-17 01:45:44 -0300

    Criando tabela contatos

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_17_044439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "address_type"
+    t.string "street_name"
+    t.string "reference"
+    t.string "zip_code"
+    t.string "city"
+    t.string "neighborhood"
+    t.string "address"
+    t.string "number"
+    t.string "complement"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "type"
+    t.string "first_name"
+    t.string "how_find_us"
+    t.string "nickname"
+    t.string "cpf"
+    t.string "rg"
+    t.date "birth_date"
+    t.string "legal_name"
+    t.string "trade_name"
+    t.string "cnpj"
+    t.string "ie"
+    t.date "foundation_date"
+    t.boolean "has_partnership"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "ctype"
+    t.string "cvalue"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
