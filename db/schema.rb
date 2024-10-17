d92d1410ecaa8e5a944f569d967358f4782d45e3
AlansidneyJunior <alansidney.silva21@gmail.com>
2024-10-17 00:32:42 -0300

    Criando tabela pai Client usando STI

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_17_031706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "type"
+    t.string "first_name"
+    t.string "how_find_us"
+    t.string "nickname"
+    t.string "cpf"
+    t.string "rg"
+    t.date "birth_date"
+    t.string "legal_name"
+    t.string "trade_name"
+    t.string "cnpj"
+    t.string "ie"
+    t.date "foundation_date"
+    t.boolean "has_partnership"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
